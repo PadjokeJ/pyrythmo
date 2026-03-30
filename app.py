42912d5fb9f8a7968395e55430b9a8a7d8ea85fa
PadjokeJ <jonatan@padjokej.dev>
2026-03-30 12:20:31 +0200

    feat: statistics !

--- app.py
+++ app.py
@@ -5,13 +5,51 @@ from flask_limiter.util import get_remote_address
 from random import random
 from os import remove, system
 
+import json
+
 import main
 
 app = Flask(__name__)
 limiter = Limiter(get_remote_address, app=app)
 
+def increment(stat):
+  data = {}
+  with open("stats/stats.json", 'r') as f:
+    data = json.load(f)
+
+  if stat in data:
+    data[stat] = data[stat] + 1
+  else:
+    data[stat] = 0
+
+  with open("stats/stats.json", 'w') as f:
+    json.dump(data, f)
+
+  return data[stat]
+
+
+@app.route("/stats")
+def stats():
+  data = {}
+  try:
+    with open("stats/stats.json", 'r') as f:
+      data = json.load(f)
+  except:
+    ...
+  
+  if "hits" not in data:
+    data["hits"] = 0
+  if "renders" not in data:
+    data["renders"] = 0
+  if "errors" not in data:
+    data["errors"] = 0
+
+  return data, 200
+
 @app.route("/")
 def home():
+  increment("hits")
+  
   return render_template("index.html")
 
 @app.route("/upload", methods=["POST"])
@@ -33,9 +71,12 @@ def upload():
     main.load_and_run(url)
   except:
     remove(url)
+    increment("errors")
     return {"error": "something went wrong"}, 500
   remove(url)
   system("rm -rf out" + url)
+
+  increment("renders")
 
   return send_from_directory("", url + ".mp4")
 
